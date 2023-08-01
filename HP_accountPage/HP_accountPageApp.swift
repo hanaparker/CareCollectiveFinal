20ccfadd4625c6143db8fbdf411f52210bf7e6af
Hana Parker <hanaparker@icloud.com>
2023-08-01 16:12:18 -0500

    Initial Commit

---
+++ HP_accountPage/HP_accountPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_accountPageApp.swift
+//  HP_accountPage
+//
+//  Created by Hana Parker on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct HP_accountPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
