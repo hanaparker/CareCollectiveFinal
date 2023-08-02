25075d7e00127cc7aa7f5aaf480a56c708f3c90f
Hana Parker <hanaparker@icloud.com>
2023-08-02 17:00:14 -0500

    Has 3 tabs that navigate to different parts of the app. Account page has a profile picture (no editing capabilities), a name (no editing capabilities) and a scroll bar with buttons (haven't figured out where to store the respective information yet)

--- HP_accountPage/ContentView.swift
+++ HP_accountPage/ContentView.swift
@@ -7,15 +7,199 @@
 
 import SwiftUI
 
+struct circleView : View {
+    @State var label: String
+    var body : some View{
+        ZStack{
+            Rectangle()
+                .fill(Color("sage"))
+                .frame(width: 80, height: 40)
+            Button("\(label)"){
+                
+                
+                
+            }
+            .font(.footnote)
+            .foregroundColor(Color("darkGrey"))
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.white
+            }
+            
+            .navigationTitle("Home")
+            .foregroundColor(.black)
+        }
+        
+    }
+}
+
+struct MapView: View {
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.white
+            }
+            
+            .navigationTitle("Map")
+            .foregroundColor(.black)
+        }
+        
+    }
+}
+
+struct AccountPage: View {
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.white
+                
+                VStack{
+                    ZStack{
+                        Color("lighGrey")
+                            .background(Color("lightGrey").edgesIgnoringSafeArea(.top))
+                        
+                        VStack{
+                            VStack (spacing: 5){
+                                Image("yoshi")
+                                    .resizable(resizingMode:.stretch)
+                                    .aspectRatio(contentMode:.fit)
+                                    .cornerRadius(10)
+                                    .frame(width:200, height: 300)
+                                    .clipShape(Circle())
+                                
+                                Text("Blue Yoshi")
+                                    .font(Font.custom("Optimaisplay-Book", size: 32, relativeTo: .title))
+                                   
+                                
+                            
+                                VStack{
+                                    
+                                    ScrollView(.horizontal) {
+                                        HStack(spacing: 10){
+                                                circleView(label: "My Impact")
+                                            circleView(label: "Favorites")
+                                            circleView(label: "My Posts")
+                                            circleView(label: "Settings")
+                                            circleView(label: "Terms & Co")
+                                            circleView(label: "Privacy Policy")
+                                            circleView(label: "Log Out")
+                                                
+                                            
+                                        } .padding()
+                                    } .frame(height: 100)
+                                }
+                                
+                                Spacer()
+                                
+                                
+                                ScrollView(.vertical, showsIndicators: true) {
+                                    VStack(spacing: 10) {
+                                        circleView(label: "My Impact")
+                                        circleView(label: "Favorites")
+                                    }
+                                }
+
+                                /*
+                                HStack{
+                                    Button("Settings"){
+                                        
+                                    }
+                                }
+                                 */
+                                
+                            }
+                        }
+                                
+                    }
+                }
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            }
+            
+            
+        }
+        
+    }
+}
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        TabView{
+    
+            HomeView()
+            .tabItem {
+                Image(systemName: "house")
+                Text("Home")
+            }
+            
+            MapView()
+            .tabItem {
+                Image(systemName: "location.circle")
+                Text("Map")
+            }
+            
+            AccountPage()
+            .tabItem {
+                Image(systemName: "person")
+                Text("Account Page")
+            }
         }
-        .padding()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                
+                
+                
+                
+                
+    
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
---
+++ myImpact.swift
@@ -0,0 +1,20 @@
+//
+//  myImpact.swift
+//  HP_accountPage
+//
+//  Created by Hana Parker on 8/2/23.
+//
+
+import SwiftUI
+
+struct myImpact: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct myImpact_Previews: PreviewProvider {
+    static var previews: some View {
+        myImpact()
+    }
+}
